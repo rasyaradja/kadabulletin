e31f77213b73dc6e4931a52958842e6051d8364c
Rasya <rasya.radja12@gmail.com>
2025-08-06 18:34:00 +0700

    fix pls

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  // Configuration for Vercel deployment
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
+	base: '/',
+	server: {
+		host: '::',
+		port: 8080,
+	},
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	// Configuration for Vercel deployment
+	build: {
+		outDir: 'dist',
+		assetsDir: '',
+		rollupOptions: {
+			output: {
+				manualChunks: undefined,
+			},
+		},
+	},
 });
